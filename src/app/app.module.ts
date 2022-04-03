58e38c24c3b6a8a341cd9316e0f88592d2ab479d
dnn-coder <dvivasmera1.2@gmail.com>
2022-04-03 11:55:57 -0500

    Ultima actualizacion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
+// routes
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
